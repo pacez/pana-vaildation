8ca3401462f4440675cf01ff64c9d2da02bb1ba6
Pace Zhong <pace_zhong@foxmail.com>
2015-06-11 20:36:21 +0800

    main.js
    main.js

---
+++ main.js
@@ -0,0 +1,77 @@
+require(["../../lib/jquery","../common/validate"],function($,Validate) {
+    Validate.init({
+        formId: 'form1',
+        submit: false,
+        success: function(){
+            
+        },
+        rules: [
+            {
+                selector: 'input[name="realName"]',
+                rule: {
+                    rangeLength: {
+                        minLength: 1,
+                        maxLength: 10
+                    }
+                }
+            },
+            {
+                selector: 'input[name="sexy"]',
+                rule: {
+                    required: true
+                }
+            },
+            {
+                selector: 'input[name="idCard"]',
+                rule: {
+                    required: true,
+                    idCard: true
+                }
+            },
+            {
+                selector: 'input[name="email"]',
+                rule:　{
+                    required: true,
+                    email: true
+                }
+            },
+            {
+                selector: 'select[name="state"]',
+                rule:　{
+                    required: true
+                }
+            },
+            {
+                selector: 'select[name="city"]',
+                rule:　{
+                    required: true
+                }
+            },
+            {
+                selector: 'input[name="address"]',
+                rule:　{
+                    required: true
+                }
+            },
+            {
+                selector: 'input[name="zipCode"]',
+                rule:　{
+                    zipCode: true
+                }
+            },
+            {
+                selector: 'input[name="birthday"]',
+                rule: {
+                    required: true,
+                    dateISO: true
+                }
+            },
+            {
+                selector: 'input[name="hasBaby"]',
+                rule: {
+                    required: true
+                }
+            }
+        ]
+    });
+});
